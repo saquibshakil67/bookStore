640f2a9e36156ef15f92c4e1c97332ecd751593c
Saquib Shakil <shakilsaquib67@gmail.com>
2024-11-08 22:58:20 +0530

    typo errir fixed

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://https://bookbinge.vercel.app'],
+    origin: ['http://localhost:5173', 'https://bookbinge.vercel.app'],
     credentials: true
 }))
 
